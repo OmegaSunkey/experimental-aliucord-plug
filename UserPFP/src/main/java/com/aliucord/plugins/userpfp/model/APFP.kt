1dbdfa0fa4ee68237a2d8b8e5bab45f80faddcdb
OmegaSunkey <bronytecfan@gmail.com>
2023-09-24 17:23:47 -0500

    first try to new database

--- UserPFP/src/main/java/com/aliucord/plugins/userpfp/model/APFP.kt
+++ UserPFP/src/main/java/com/aliucord/plugins/userpfp/model/APFP.kt
@@ -15,8 +15,8 @@ import com.discord.utilities.images.MGImages
 
 
 object APFP : AbstractDatabase() {
-    override val regex: String = ".*(https:\\/\\/.*?\\.gif).*(https:\\/\\/.*?\\.png|jpg)"
-    override val url: String = "https://raw.githubusercontent.com/OmegaSunkey/UserPFP-Discord/main/UserPFP.txt"
+    override val regex: String = "\"\\]\\).*?(https:\\/\\/[\\w.\\/-]*)" //.*(https:\\/\\/.*?\\.gif).*(https:\\/\\/.*?\\.png|jpg)
+    override val url: String = "https://raw.githubusercontent.com/Yeetov/USRPFP-Reborn/main/db/dist.css" //https://raw.githubusercontent.com/OmegaSunkey/UserPFP-Discord/main/UserPFP.txt
 
     override var data: String = ""
 
@@ -39,35 +39,22 @@ object APFP : AbstractDatabase() {
                     )) return@Hook
                 val id = it.args[0] as Long
                 if (mapCache.containsKey(id))
-                    it.result = mapCache[id]?.let { it1 ->  if ((it.args[3] as Boolean)) it1.animated else it1.static
+                    it.result = mapCache[id]?.let { it1 ->  it1.animated
                 } else {
                     val matcher = Pattern.compile(
                         id.toString() + regex,
                         Pattern.DOTALL
                     ).matcher(data)
                     if (matcher.find()) {
-                        mapCache[id] = PFP(matcher.group(2), matcher.group(1)).also {
-                                it1 ->  if ((it.args[3] as Boolean)) it.result = it1.animated else it.result = it1.static
+                        mapCache[id] = PFP(matcher.group(1)).also {
+                                it1 ->  it.result = it1.animated
                         }
                     }
                 }
 
             }
         )
-
-        patcher.patch(
-            IconUtils::class.java.getDeclaredMethod("setIcon", ImageView::class.java, String::class.java, Int::class.javaPrimitiveType, Int::class.javaPrimitiveType, Boolean::class.javaPrimitiveType, Function1::class.java, MGImages.ChangeDetector::class.java), Hook {
-                if ((it.args[1] as String).contains("https://cdn.discordapp.com/role-icons")) return@Hook
-
-                val simpleDraweeView = it.args[0] as SimpleDraweeView
-                simpleDraweeView.apply {
-                    hierarchy.n(s.l)
-                    clipToOutline = true
-                    background =
-                        ShapeDrawable(OvalShape()).apply { paint.color = Color.TRANSPARENT }
-                }
-            })
     }
 
-    data class PFP(val static: String, val animated: String)
+    data class PFP(val animated: String)
 }
